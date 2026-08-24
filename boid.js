cb03cf2b1009f6212a0cffdcfe97ade3d0d72c54
Leander Maigler <l.maigler@comforts.de>
2026-08-24 14:07:38 +0200

    adding cohesion with tutorial

--- boid.js
+++ boid.js
@@ -2,8 +2,25 @@ class Boid{
     constructor(){
         this.position = createVector(random(width), random(height));
         this.velocity = p5.Vector.random2D();
+        this.velocity.setMag(random(2, 4));
         this.acceleration = createVector();
+        this.maxForce = 0.2;
+        this.maxSpeed = 2;
     }
+
+    edges(){
+        if(this.position.x > width){
+            this.position.x = 0;
+        } else if(this.position.x < 0) {
+            this.position.x = width;
+        }
+        if(this.position.y > height){
+            this.position.y = 0;
+        } else if(this.position.y < 0){
+            this.position.y = height;
+        }
+    }
+    
 
     // allign boids with local boids around it
     align(boids){
@@ -31,18 +48,48 @@ class Boid{
         return steering;
     }
 
+    cohesion(boids){
+        let perception = 50;
+        let steering = createVector();
+        let total = 0;
+        for(let other of boids){
+            let distance = distance(
+                this.position.x,
+                this.position.y,
+                other.position.x,
+                other.position.y
+            );
+            if(other != this && distance < perception){
+                steering.add(other.position);
+                toatl++;
+            }
+            if(total > 0){
+                steering.div(total);
+                steering.sub(this.position);
+                steering.setMag(this.maxSpeed);
+                steering.sub(this.velocity);
+                steering.limit(this.maxforce);
+            }
+            return steering;
+        }
+    }
+
     flock(boid){
+        this.acceleration.set(0, 0);
         let alignment = align(boids);
-        this.acceleration = alignment;
+        let cohesion = cohesion(boids);
+        this.acceleration = add(alignment);
+        this.acceleration = add(cohesion);
     }
 
     update(){
         this.position.add(this.velocity);
         this.velocity.add(this.acceleration);
+        this.velocity.maxSpeed(this.maxSpeed);
 
     }
     show(){
-        strokeWeight(16);
+        strokeWeight(8);
         stroke(255);
         point(this.position.x, this.position.y);
     }
